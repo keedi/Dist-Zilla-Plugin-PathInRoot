b981e9a229be3e13e4e72f93d5b18985464cca3f
Keedi Kim <keedi.k@gmail.com>
2013-12-24 15:59:10 +0900

    Add 'copy' config attribute

--- lib/Dist/Zilla/Plugin/PathInRoot.pm
+++ lib/Dist/Zilla/Plugin/PathInRoot.pm
@@ -1,6 +1,30 @@
 package Dist::Zilla::Plugin::PathInRoot;
 # ABSTRACT: Puts the build files in the project root
 
+use Moose;
+
+use namespace::autoclean;
+
+sub mvp_multivalue_args { qw( paths_to_copy ) }
+sub mvp_aliases         { +{ copy => 'paths_to_copy' } }
+
+has paths_to_copy => (
+    is       => 'ro',
+    isa      => 'ArrayRef[Str]',
+    required => 1,
+);
+
+around dump_config => sub {
+    my ( $orig, $self ) = @_;
+    my $config = $self->$orig;
+
+    my $this_config = { paths_to_copy => $self->paths_to_copy, };
+
+    $config->{ '' . __PACKAGE__ } = $this_config;
+
+    return $config;
+};
+
 1;
 __END__
 
